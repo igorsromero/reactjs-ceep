d82e348a96c4df73bcdc1ad79bb1a901486110af
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-24 11:53:16 -0300

    Criando Componentes

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    <section>
+                        <header>
+                            <h3>Título</h3>
+                        </header>
+                        <p>Escreva sua nota</p>
+                    </section>
+                </li>
+            </ul>
+        );
+    }
+}
