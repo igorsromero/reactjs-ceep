ec9a596a219b352fcc77e77ef88edd96c5b776b0
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-24 13:39:17 -0300

    Lidando com evento e props

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import "./estilo.css"
 
 class FormularioCadastro extends Component {
+    constructor() {
+        super();
+        this.titulo = "";
+    }
+
+    handleMudancaTitulo(evento) {
+        this.titulo = (evento.target.value);
+        console.log(this.titulo);
+    }
     render() {
         return (
             <form className="form-cadastro">
-                <input type="text" placeholder="Título" className="form-cadastro_input" />
-                <textarea rows={15} placeholder="Escreva sua nota..." className="form-cadastro_input"></textarea>
-                <button className="formcadastro_input form-cadastro-submit">Criar Nota</button>
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                    onChange={this.handleMudancaTitulo.bind(this)}
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                    className="form-cadastro_input">
+                </textarea>
+                <button className="formcadastro_input form-cadastro-submit">
+                    Criar Nota
+                </button>
             </form>
         );
     };
